0809d7a1abfd3f8d9ba674af01308e5b334c9c15
Tim Andersson <timalexanderandersson@hotmail.com>
2024-04-23 12:35:44 +0000

    Pull in the elements in javascript

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,6 @@
+let TextInputArea = document.getElementById('textareahere');
+let AddButton = document.getElementById('theaddbutton');
+let HereGoesText = document.getElementById('HereGoesTheTask');
+
+
+
