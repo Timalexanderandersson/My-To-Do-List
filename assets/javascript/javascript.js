4dd9b1cb254f8ec2beb217aa184da8a83bfe6cd5
Tim Andersson <timalexanderandersson@hotmail.com>
2024-04-23 13:25:35 +0000

    made AddYourTask Function and EventListener for it

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,31 @@
+// elements in from HTML.
+
 let TextInputArea = document.getElementById('textareahere');
 let AddButton = document.getElementById('theaddbutton');
 let HereGoesText = document.getElementById('HereGoesTheTask');
 
+
+
+
+// Function for adding the Task.
+function AddYourTask() {
+
+    let ThetextValue = TextInputArea.value;
+
+    let TheDeleteButton = document.createElement('button');
+    TheDeleteButton.innerText = 'Delete';
+    let TheNewListTask = document.createElement('li');
+
+    TheNewListTask.innerText = ThetextValue;
+    HereGoesText.appendChild(TheNewListTask);
+    TheNewListTask.appendChild(TheDeleteButton);
+
+    console.log('AddYourTask working')
+};
+
+// EventListener for AddYourTask function.
+AddButton.addEventListener('click', AddYourTask,
+    console.log('event working')
+);
 
 
